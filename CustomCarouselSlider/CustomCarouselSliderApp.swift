bbfda12628666ed6378de09db807b63bc791e8a4
SabbirHossain07 <sabbirsopnil07@gmail.com>
2021-09-21 20:36:41 +0600

    Initial Commit

---
+++ CustomCarouselSlider/CustomCarouselSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCarouselSliderApp.swift
+//  CustomCarouselSlider
+//
+//  Created by Sopnil Sohan on 21/9/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomCarouselSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
